0b4b812e86f2dc064c692db1ac63fb0ba8fc5963
Tangjj1996 <awesometangji@gmail.com>
2024-09-20 23:10:48 +0800

    feat: 增加 house 和 user 模块

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { HouseModule } from './house/house.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, HouseModule],
   controllers: [AppController],
   providers: [AppService],
 })
