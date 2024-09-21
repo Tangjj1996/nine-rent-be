cd936a527264748539b36ecaa556987e771b62a6
Tangjj1996 <awesometangji@gmail.com>
2024-09-21 09:29:32 +0800

    feat: 连接数据库

---
+++ src/core/interceptor/transform/transform.interceptor.ts
@@ -0,0 +1,22 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  HttpStatus,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+
+@Injectable()
+export class TransformInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => ({
+        data,
+        code: HttpStatus.OK,
+        msg: 'ok',
+        traceId: new Date().toLocaleString(),
+      })),
+    );
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './core/filter/http-exception/http-exception.filter';
+import { TransformInterceptor } from './core/interceptor/transform/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // 全局前缀
+  app.setGlobalPrefix('api');
+  // 使用全局验证管道
+  app.useGlobalPipes(new ValidationPipe());
+  // 注册全局错误的过滤器
+  app.useGlobalFilters(new HttpExceptionFilter());
+  // 注册全局拦截器
+  app.useGlobalInterceptors(new TransformInterceptor());
+  // 允许跨域
+  app.enableCors();
+  await app.listen(3001);
 }
 bootstrap();
